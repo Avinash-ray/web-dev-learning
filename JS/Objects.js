3b0c349dc01ffbf5cd14573e3a3403ccdf00aed4
Avinash-ray <rayavinash616@gmail.com>
2022-09-13 08:16:53 +0530

    objects key access

--- JS/Objects.js
+++ JS/Objects.js
@@ -12,3 +12,27 @@ console.log(person.gender);
 
 // square bracket notation
 console.log(person["age"]);
+
+// sign up
+console.log(`hey ${person.name} thank you for signing up`)
+
+let captainAmerica= {
+    firstName: "Steve",
+    lastName: "Rogers",
+    friends: ["Bucky", "Tony Stark", "Breuce Banner"],
+    age: 10,
+    isAvenger: true,
+    address: {                      //nesting of objects
+        state: "Mahattan",
+        City: "New York",
+        Country: "USA"
+    },
+    SayHi: function(){
+        console.log(`hello my name is ${this.firstName}`);
+    }
+}
+
+console.log(captainAmerica);
+console.log(captainAmerica.friends);
+console.log(captainAmerica.friends[0]);
+captainAmerica.SayHi();
