ea9d932232c7844bddc4a9ec8dad306ac1b8e143
Avinash-ray <rayavinash616@gmail.com>
2022-09-13 07:42:03 +0530

    Objects started (dot & bracket notation)

---
+++ JS/Objects.js
@@ -0,0 +1,14 @@
+let obj={}
+console.log(obj)
+
+let person= {
+    name: "Avinash",
+    age: 28,
+    gender: "male"
+}
+
+// dot notation
+console.log(person.gender);
+
+// square bracket notation
+console.log(person["age"]);
