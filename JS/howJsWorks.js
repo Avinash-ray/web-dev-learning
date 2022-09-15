a72bc33866f20edff2ef0c07ba2c370ee3db3609
Avinash-ray <rayavinash616@gmail.com>
2022-09-15 11:13:43 +0530

    how js works

---
+++ JS/howJsWorks.js
@@ -0,0 +1,14 @@
+
+var num= 5;
+
+function cube(num){
+    var res= num*num*num
+    return res;
+}
+var cube1= cube(num);
+var cube2= cube();
+
+
+console.log(" "+cube)
+console.log(cube2);
+console.log(undefined *5 * undefined);
