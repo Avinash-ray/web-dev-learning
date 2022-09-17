94a8f611d043d7790c73fc7d36f217c51f2da52a
Avinash-ray <rayavinash616@gmail.com>
2022-09-17 13:12:25 +0530

    hoisting

---
+++ JS/hoisting.js
@@ -0,0 +1,13 @@
+// var x=10;
+// function greet(){
+//     console.log("pepcoders");
+// }
+// console.log(x);
+// greet();
+
+console.log(a);
+greet();
+var a= 10;
+function greet(){
+    console.log("pepcoders")
+}
