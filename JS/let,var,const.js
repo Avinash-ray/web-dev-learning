92b4383dedd59ca00cd28d7ca940e644a845f7d5
Avinash-ray <rayavinash616@gmail.com>
2022-10-07 16:04:59 +0530

    blockscope and shadowing

--- JS/let,var,const.js
+++ JS/let,var,const.js
@@ -1,4 +1,33 @@
-console.log(x)
-console.log(b)
-var x= 10;
-const b= 100;
+// TDZ
+
+// console.log(x)
+// console.log(b)
+// var x= 10;
+// const b= 100;
+
+// block scope
+
+// var a= 100
+// {
+//     var a= 10;
+//     let b= 20;
+//     const c= 30;
+
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+// console.log(a);
+// console.log(b);
+
+let a= 100
+{
+    var b= 10;
+    let a= 20;
+    const c= 30;
+
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+console.log(a);
