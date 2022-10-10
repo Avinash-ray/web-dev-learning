d83eadad38c63882ab8799968349408e6adfc939
Avinash-ray <rayavinash616@gmail.com>
2022-10-10 21:24:36 +0530

    learned about require

---
+++ JS/temp.js
@@ -0,0 +1,15 @@
+let obj= {
+add: function add(a,b){
+    return a+b;
+},
+
+sub: function subtr(a,b){
+    return a-b;
+},
+
+hello: 123
+}
+
+console.log(obj)
+
+module.exports= obj;
---
+++ NodeJs/require.js
@@ -0,0 +1,4 @@
+let abc= require("../JS/temp")
+console.log(abc)
+let ans= abc.add(2,4)
+ console.log(ans);
