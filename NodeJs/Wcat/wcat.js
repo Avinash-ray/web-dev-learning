413a520461f2c1f2ff0ef25565028e0d4d5eeee7
Avinash-ray <rayavinash616@gmail.com>
2022-10-12 12:45:43 +0530

    wcat project completed

--- NodeJs/Wcat/wcat.js
+++ NodeJs/Wcat/wcat.js
@@ -1,7 +1,10 @@
 /*1). node wcat.js filepath => display content of a file
  2). node wcat.js filepath1 filepath2 filepath3 =>displays the content of all files in terminal in concatenated
 form in given order 
-3). node wcat.js -s filepath => change multiple line breaks into singular
+3). node wcat.js -s filepath(s) => change multiple line breaks into singular
+4). node wcat.js -n filepath(s) => give numbering to all lines.
+5). node wcat.js -b filepath(s) => give numbering to only non empty lines
+We can mix and match the options.
 
 */
 
@@ -46,6 +49,7 @@ console.table(contentArr);
 
 // check if -s is present or not
 let istrue= optionsArr.includes("-s");
+let tempArr= [];
 if(istrue){
     for(let i=1; i< contentArr.length; i++){
         if(contentArr[i] =="" && contentArr[i-1] ==""){
@@ -55,13 +59,61 @@ if(istrue){
             contentArr[i] = null;
         }
     }
-}
+    console.table(contentArr)
 
-let tempArr= [];
-// push everytjing in tempArr except null
+// var tempArr= [];
+// push everything in tempArr except null
 for(let i=0; i< contentArr.length;i++){
     if(contentArr[i] != null){
         tempArr.push(contentArr[i])
     }
 }
 console.log("data after removing extra lines\n",tempArr)
+}
+contentArr = tempArr;
+
+let indexOFN= optionsArr.indexOf("-n")
+let indexOFB= optionsArr.indexOf("-b")
+//  if -n or -b is not found, return -1.
+
+let finalOption= ""
+// if -a and -b are present
+if(indexOFN != -1 && indexOFB != -1){
+    if(indexOFN < indexOFB){
+        finalOption= "-n"
+    }
+    else{
+        finalOption= '-b'
+    }
+}
+// either -n or -b is present
+if(indexOFN != -1){
+    finalOption= "-n"
+}
+else if(indexOFB != "-b"){
+    finalOption= "-b"
+}
+
+// calling of functions by evaluating of finalOption
+if(finalOption == "-n"){
+    contentmodifybyN()
+}
+else if(finalOption == "-b"){
+    contentmodifybyB();
+}
+
+function contentmodifybyN(){
+    for(let i=0; i< contentArr.length; i++){
+        contentArr[i]= (i+1)+") "+contentArr[i]
+    }
+}
+
+function contentmodifybyB(){
+    let count= 1;
+    for(let i=0; i< contentArr.length; i++){
+        contentArr[i]= count+") "+contentArr[i]
+        count ++
+    }
+}
+console.log(contentArr)
+
